ad8b9fbc2a0ff02560859245e350d978aa43b9b4
Craig Sloggett <craig@infostrux.com>
2022-10-17 11:40:05 -0400

    feat: Populate the snowflake_user resources with configuration found in spec.yml

--- locals.tf
+++ locals.tf
@@ -2,4 +2,27 @@ locals {
   # Constants
 
   spec = yamldecode(file(var.spec_file_path))
+
+  default_user_spec = {
+    name                    = ""
+    comment                 = ""
+    default_namespace       = ""
+    default_role            = ""
+    default_secondary_roles = []
+    default_warehouse       = ""
+    disabled                = false
+    display_name            = ""
+    email                   = ""
+    first_name              = ""
+    last_name               = ""
+    login_name              = ""
+    must_change_password    = true
+    password                = ""
+    rsa_public_key          = ""
+    rsa_public_key_2        = ""
+  }
+
+  users = {
+    for user, spec in local.spec["users"] : user => merge(local.default_user_spec, spec)
+  }
 }
--- users.tf
+++ users.tf
@@ -1,3 +1,19 @@
-#resource "snowflake_user" "users" {
-#  # Populate Me
-#}
+resource "snowflake_user" "users" {
+  for_each                = local.users
+  name                    = each.value.name
+  comment                 = each.value.comment
+  default_namespace       = each.value.default_namespace
+  default_role            = each.value.default_role
+  default_secondary_roles = each.value.default_secondary_roles
+  default_warehouse       = each.value.default_warehouse
+  disabled                = each.value.disabled
+  display_name            = each.value.display_name
+  email                   = each.value.email
+  first_name              = each.value.first_name
+  last_name               = each.value.last_name
+  login_name              = each.value.login_name
+  must_change_password    = each.value.must_change_password
+  password                = each.value.password
+  rsa_public_key          = each.value.rsa_public_key
+  rsa_public_key_2        = each.value.rsa_public_key_2
+}
